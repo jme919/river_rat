d500e003c966a676a278ceaf5721eaf9dc202bdb
Mariea Johnson <marieaajohnson@gmail.com>
2018-06-22 20:29:40 -0400

    Created NavBar Component

---
+++ client/src/components/MainPage/MainPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./MainPage.css";
+import { MainPage } from "./MainPage";
+
+export class MainPage extends Component {
+  render() {
+    return (
+      <div className="MainPage">
+        <MainPage/>
+      </div>
+    );
+  }
+}
