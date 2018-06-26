201d2890c1a5bef6f511e47aa858a1a677f27591
Unknown <jme919@gmail.com>
2018-06-25 20:55:34 -0400

    add mongoose to yarn, add email verification, add password verification, add route for submitting user data form

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose");
+
+
+//Make sure that this is the correct thing to do here
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/river_rat");
+
 
 // Define middleware here
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,6 +19,20 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Define API routes here
+// Route to post our form submission to mongoDB via mongoose
+app.post("/submit", function (req, res) {
+  // Create a new user using req.body
+  User.create(req.body)
+    .then(function (dbUser) {
+      // If saved successfully, send the the new User document to the client
+      res.json(dbUser);
+    })
+    .catch(function (err) {
+      // If an error occurs, send the error to the client
+      res.json(err);
+    });
+});
+
 
 // Send every other request to the React app
 // Define any API routes before this runs
