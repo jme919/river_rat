30918cd5c9b7792e8f8f0eb6f01d7daf058e5c10
Unknown <jme919@gmail.com>
2018-07-03 19:44:37 -0400

    add font families, write route for park page, add api route to try and render park data

--- client/src/pages/ParkPage/ParkPage.js
+++ client/src/pages/ParkPage/ParkPage.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, {Component} from "react";
+import parks from "../../parks.json";
+import ParkWrap from "./../../components/ParkWrap";
+import API from "./../../utils/API";
+class ParkPage extends Component {
+   state = {
+     parkArr: parks,
+     park:""
+   }
+componentDidMount =()=>{
+  API.getPark().then(res => { 
+    console.log(res);
+    this.setState({park:res.park})
+   })
+  .catch(err => console.log(err))
 
-const ParkPage = (props) => (
-  <div className="parkPage">ParkPage</div>
-);
+}
+  render() {
+    return(
+      <ParkWrap>
+      <ul>
+      {this.state.parkArr.map((park)=>
+      <li>{park.name}</li>
+      )}
+      </ul>
+
+      </ParkWrap>
+
+    );
+  }
+}
 
 export default ParkPage;
--- client/src/pages/ParksPage/ParksPage.js
+++ client/src/pages/ParksPage/ParksPage.js
@@ -1,17 +1,16 @@
 import React, { Component } from "react";
 // import child component
 import MapContainer from "../../components/GoogleApi/MapContainer";
-// import parks from "./parks.json";
+
 // IMPORTANT: Passing the Google Maps props down to the MapContainer component as "google".
 
 class ParksPage extends Component {
-  // state = {
-  //   parkArr: parks,
-  // }
+ 
   render() {
     return (
       <div>
         <h1>Parks</h1>
+        
         <MapContainer />
       </div>
     );
