6ed84dc6ee21678484819593e228f43a4884d84b
Unknown <jme919@gmail.com>
2018-07-09 21:26:53 -0400

    start comment form, add icons to info list

--- client/src/components/Comments/Comments.js
+++ client/src/components/Comments/Comments.js
@@ -2,9 +2,38 @@ import React from "react";
 import "./Comments.css";
 
 const Comments = props => (
-    <div className="comments">
-        <p>Lorem ipsum </p>
-    </div>
+    <section>
+        <div id="comments" className="container is-fluid">
+            <div className="columns">
+                <div className="column">
+
+                    <div className="field">
+                        <label className="label is-text is-success">Discussion</label>
+                        <div className="control has-icons-left has-icons-right">
+                            <input className="input is-success" type="text" placeholder="Text input" value="Username" />
+                            <span className="icon is-small is-left">
+                                <i className="fas fa-user"></i>
+                            </span>
+                        </div>
+
+                    </div>
+                    <div className="field">
+                        <div className="control is-large">
+                            <textarea className="textarea is-success" type="text" placeholder="Leave a tip, info, or a review of this park!"></textarea>
+
+                        </div>
+                    </div>
+                    <div className="field is-grouped">
+                        <div className="control">
+                            <button className="button  is-success">Submit</button>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
 )
 
 export default Comments
