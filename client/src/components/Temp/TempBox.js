9a82b4ba6f815ba42f8ff1718d480be585e91e18
Johnny <johnnyheath@hotmail.com>
2018-07-13 23:45:42 -0400

    Updating data on home page to float in on page load/started adding icons

--- client/src/components/Temp/TempBox.js
+++ client/src/components/Temp/TempBox.js
@@ -1,10 +1,12 @@
 import React from "react"
 import "./TempBox.css"
 import LevelBox from "../LevelBox";
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const TempBox = props => (
     <div className="temp-databox">
-        <p className="temp-heading">Temperature</p>
+        <p className="temp-heading" >Temperature</p>
+        {/* <FontAwesomeIcon icon="fa-thermometer-three-quarters" />" /> */}
         <p className="temp-data">{(props.temp * 1.8 + 32).toFixed(2)}</p>
         <p className="temp-definition">&deg;F</p>
     </div>    
