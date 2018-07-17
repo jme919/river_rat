0fa420b4e033b5025c40adae6789e341e848aa5f
Mariea Johnson <marieaajohnson@gmail.com>
2018-07-16 20:56:22 -0400

    add icons to main page and adjust responsiveness

--- client/src/components/Clarity/ClarityBox.js
+++ client/src/components/Clarity/ClarityBox.js
@@ -3,6 +3,7 @@ import "./ClarityBox.css"
 
 const ClarityBox = props => (
     <div className="clarity-databox">
+        <i className="fas fa-adjust"/>
         <p className="clarity-heading">Clarity</p>
         <p className="clarity-data">{props.clarity}</p>
         <p className="clarity-defintion">{"<10 is good"}</p>
